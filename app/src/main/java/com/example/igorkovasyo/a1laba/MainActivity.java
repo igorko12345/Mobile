6dab6faeccc38281592a9b87ae3dc9ab9e95e4b9
igorko12345 <igor.vasyo.199988@gmail.com>
2018-09-12 16:10:29 +0300

    2laba

--- app/src/main/java/com/example/igorkovasyo/a1laba/MainActivity.java
+++ app/src/main/java/com/example/igorkovasyo/a1laba/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.igorkovasyo.a1laba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonHello, buttonClear;
+    EditText inputName;
+    TextView myName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inputName = findViewById(R.id.inputName);
+        buttonClear = findViewById(R.id.buttonClear);
+        buttonHello = findViewById(R.id.buttonHello);
+        myName = findViewById(R.id.myName);
     }
-}
+
+    public void clearMessage(View view) {
+        inputName.getText().clear();
+    }
+
+    public void showMessage(View view) {
+        myName.setText("Hello" + " " + inputName.getText().toString());
+        inputName.getText().clear();
+    }
+
+    }
